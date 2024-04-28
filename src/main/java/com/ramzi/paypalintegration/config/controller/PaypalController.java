1df5dfeaad06bc68dacc8dee41868d23bdc2e475
Ramzi BOUZAIENE <ramzi.bouzaiene@proxym-it.com>
2024-04-28 15:55:17 +0100

    adding paypal html templates and updating the controller

--- src/main/java/com/ramzi/paypalintegration/config/controller/PaypalController.java
+++ src/main/java/com/ramzi/paypalintegration/config/controller/PaypalController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -23,17 +24,22 @@ public class PaypalController {
     }
 
     @PostMapping("/payment/create")
-    public RedirectView createPayment(){
+    public RedirectView createPayment(
+            @RequestParam("method") String method,
+            @RequestParam("amount") String amount,
+            @RequestParam("currency") String currency,
+            @RequestParam("description") String description
+    ){
         try{
             String cancelUrl = "http://localhost:8080/payment/cancel";
             String successUrl = "http://localhost:8080/payment/success";
 
             Payment payment = paypalService.createPayment(
-                    20.0,
-                    "USD",
-                    "paypal",
+                    Double.valueOf(amount),
+                    currency,
+                    method,
                     "sale",
-                    "payment desc",
+                    description,
                     cancelUrl,
                     successUrl
             );
@@ -48,5 +54,31 @@ public class PaypalController {
             log.error("Error occurred :", e);
         }
         return new RedirectView("payment/error");
+    }
+
+    @GetMapping("/payment/success")
+    public String paymentSuccess(
+            @RequestParam("pymentId") String paymentId,
+            @RequestParam("payerId") String payerId
+    ){
+        try{
+            Payment payment = paypalService.executePayment(paymentId, payerId);
+            if(payment.getState().equals("approved")){
+                return "paymentSuccess";
+            }
+        }catch (PayPalRESTException e){
+            log.error("Error occurred: ", e);
+        }
+        return "paymentSuccess";
+    }
+
+    @GetMapping("/payment/cancel")
+    public String paymentCancel(){
+        return "paymentCancel";
+    }
+
+    @GetMapping("/payment/error")
+    public String paymentError(){
+        return "paymentError";
     }
 }
